f22dabb363e90dcee0d4d03faad6dbeef5935301
ChristopherAChase <Christopher.A.Chase@outlook.com>
2021-03-29 14:46:36 -0500

    returned array as JSON object

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def get_assignment():
 
     # print(f'username: {username} \n password: {password} ')
     if username and password:
-        return scrape.main(username, password)
+        return jsonify(scrape.main(username, password))
     return {'result': 'Need both the username and password'}
 
 
