b471758ce9b892722403bf9b2c8054922223c771
ChristopherAChase <Christopher.A.Chase@outlook.com>
2021-03-29 15:36:53 -0500

    returned array as JSON object

--- app.py
+++ app.py
@@ -2,18 +2,19 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from scripts import scrape
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 
 
 @app.route('/assignments/', methods=['GET', 'POST'])
+@cross_origin(supports_credentials=True)
 def get_assignment():
     username = request.args.get("username", None)
     password = request.args.get("password", None)
 
     # print(f'username: {username} \n password: {password} ')
     if username and password:
-        return scrape.main(username, password)
-    return {'result': 'Need both the username and password'}
+        return jsonify(scrape.main(username, password))
+    return jsonify({'result': 'Need both the username and password'})
 
 
 @app.route('/')
