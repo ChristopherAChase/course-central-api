ec59835c9afc5914b448488f17c67923c13a9fe8
ChristopherAChase <Christopher.A.Chase@outlook.com>
2021-03-29 14:52:30 -0500

    Enabled CORS

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from scripts import scrape
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 
 @app.route('/assignments/', methods=['GET', 'POST'])
+@cross_origin(supports_credentials=True)
 def get_assignment():
     username = request.args.get("username", None)
     password = request.args.get("password", None)
