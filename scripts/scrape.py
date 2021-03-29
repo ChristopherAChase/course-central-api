0364a50e9dcf73487ab3472526d4279680517ccd
ChristopherAChase <Christopher.A.Chase@outlook.com>
2021-03-29 14:28:24 -0500

    refactored and now retrieve grade and class info

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,20 +1,14 @@
-#!/usr/bin/env python
-
-from bs4 import BeautifulSoup
 import mechanicalsoup
 from base64 import b64decode
 from nacl.secret import SecretBox
-import json
-
-REMOVE_ATTRIBUTES = [
-    'lang', 'language', 'onmouseover', 'onmouseout', 'script', 'style', 'font',
-    'dir', 'face', 'size', 'color', 'style', 'class', 'width', 'height', 'hspace',
-    'border', 'valign', 'align', 'background', 'bgcolor', 'text', 'link', 'vlink',
-    'alink', 'cellpadding', 'cellspacing']
+from . import scrapeutils as scraper
+import os
 
 BASE_URL = 'https://stinet.southeasttech.edu'
 
-SECRET_KEY = '_THIS_IS_MY_32_CHARS_SECRET_KEY_'
+# SECRET_KEY = os.environ.get('SECRET_KEY', None)
+SECRET_KEY = 'b10dcd22075319347f196d65e1e61f6a'
+
 
 def main(username, password):
     encrypted_password = password.split(':')
@@ -27,7 +21,7 @@ def main(username, password):
     box = SecretBox(bytes(SECRET_KEY, encoding='utf8'))
     decrypted_password = box.decrypt(encrypted, nonce).decode('utf-8')
 
-    class_data = {}
+    class_data = []
 
     # Create a browser object
     browser = mechanicalsoup.StatefulBrowser()
@@ -58,9 +52,11 @@ def main(username, password):
 
     # Loops through each class in list and gets the name, and the url and adds it to an object
     for course in courses.find_all('a'):
+        class_url_base = f"{BASE_URL}{course['href']}"
         class_name = course.text.replace('  ', ' ')
-        class_url = f"{BASE_URL}{course['href']}Coursework.jnz"
-
+        class_url = f"{class_url_base}Coursework.jnz"
+        class_grade = scraper.getCurrentGrade(class_url_base, browser)
+        class_summary = scraper.getClassInfo(class_url_base, browser)
         # Open the coursework page for current class
         browser.open(class_url)
         coursework = browser.get_current_page()
@@ -76,57 +72,21 @@ def main(username, password):
             assignment = {
                 'name': assignment.find('a').text,
                 'due_date': assignment.find('strong').text,
-                'link': f"{BASE_URL}{assignment.find('a')['href']}"
+                'link': f"{BASE_URL}{assignment.find('a')['href']}",
+                'instructions': scraper.getAssignmentInstructions(f"{BASE_URL}{assignment.find('a')['href']}", browser),
+                'files': scraper.getAssignmentFiles(f"{BASE_URL}{assignment.find('a')['href']}", browser)
             }
 
-            # Go to the assignment's specific page
-            browser.open(assignment['link'])
-            assignment_page = browser.get_current_page()
-
-            # Go to the instructions section and gather additional instructions if they exist
-            instructions = assignment_page.find(
-                'div', class_='studentAssignmentInfo').find_all('div', class_='wysiwygtext')
-
-            # Create a beautifulSoup object to store entire children elements
-            instruction_content = BeautifulSoup()
-
-            # Loop through elements that contain instruction content
-            for section in instructions:
-                # Within all elements that contain instruction content, loop through all direct children
-                for child in section.find_all(True, recursive=False):
-                    # remove unicode encoding, and strip text of element to check if element is empty
-                    if ''.join([i if ord(i) < 128 else ' ' for i in child.text]).strip():
-                        # first strip non-empty elements of all attributes and add it to our beautifulSoup object
-                        for attribute in REMOVE_ATTRIBUTES:
-                            del child[attribute]
-                        instruction_content.append(child)
-                # convert all instruction content to a string, and strip of unicode characters, add to our assignment object
-                assignment.update({
-                    'instructions': ''.join([i if ord(i) < 128 else ' ' for i in str(instruction_content)])
-                })
-
-            # Gather file information if it exists
-            files_sect = assignment_page.find('div', class_='fileDisplay')
-            files = []
-            if files_sect:
-                # Look in all of the a tags for the file information
-                for file in files_sect.find_all('a'):
-                    # Grab the file information and add it to our files array
-                    files.append({
-                        'name': file.text,
-                        'url': f"{BASE_URL}{file['href']}"
-                    })
-                # Add our array of file objects and add it to our assignment object
-                assignment.update({'files': files})
-
-            # Add our current assignment's assignment object to our array of all assignments for the current class
             all_assignments.append(assignment)
 
         # Add the current class's information and assignments to our class_data object
-        class_data[class_name] = {
+        class_data.append({
+            'name': class_name,
+            'grade': class_grade,
+            'summary': class_summary,
             'link': class_url,
             'assignments': all_assignments
-        }
+        })
 
     # Display the current JSON object
     return str(class_data)
---
+++ scripts/scrapeutils.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+import unicodedata
+
+BASE_URL = 'https://stinet.southeasttech.edu'
+
+REMOVE_ATTRIBUTES = [
+    'lang', 'language', 'onmouseover', 'onmouseout', 'script', 'style', 'font',
+    'dir', 'face', 'size', 'color', 'style', 'class', 'width', 'height', 'hspace',
+    'border', 'valign', 'align', 'background', 'bgcolor', 'text', 'link', 'vlink',
+    'alink', 'cellpadding', 'cellspacing']
+
+
+def getClassInfo(class_URL_base, browser):
+    info_url = f"{class_URL_base}Main_Page.jnz"
+    browser.open(info_url)
+    class_info = browser.get_current_page()
+
+    # gathers the HTML of the class info section
+    class_info_section = class_info.find(class_='pi_About_This_Course') \
+        .find(class_='pSection') \
+        .find(class_='wysiwygtext') \
+        .contents
+
+    # Turns the HTML object and turns it into a string to remove all gaps between tags
+    class_info_without_gaps = str(class_info_section).replace('> <', '><')
+
+    # Turn the string of HTML tags back into an HTML object
+    class_info_section = BeautifulSoup(
+        class_info_without_gaps, features="lxml")
+
+    sections = [
+        section for section in class_info_section.find_all(name='span')]
+
+    sections = [section.get_text().strip() for section in sections]
+
+    sections = [unicodedata.normalize('NFKD', section)
+                for section in sections]
+
+    # return sections
+    return ' '.join(sections)
+
+
+def getCurrentGrade(class_url_base, browser):
+    gradebook_url = f"{class_url_base}Gradebook.jnz"
+
+    browser.open(gradebook_url)
+
+    current_grade = browser.get_current_page().find(class_='finalGradeValue').text
+
+    return current_grade
+
+
+def getAssignmentInstructions(url, browser):
+    # Go to the assignment's specific page
+    browser.open(url)
+    assignment_page = browser.get_current_page()
+
+    # Go to the instructions section and gather additional instructions if they exist
+    instructions = assignment_page.find(
+        'div', class_='studentAssignmentInfo').find_all('div', class_='wysiwygtext')
+
+    # Create a beautifulSoup object to store entire children elements
+    instruction_content = BeautifulSoup()
+
+    edited_instructions = []
+
+    # Loop through elements that contain instruction content
+    for section in instructions:
+        # Within all elements that contain instruction content, loop through all direct children
+        for child in section.find_all(True, recursive=False):
+            # remove unicode encoding, and strip text of element to check if element is empty
+            if ''.join([i if ord(i) < 128 else ' ' for i in child.text]).strip():
+                # first strip non-empty elements of all attributes and add it to our beautifulSoup object
+                for attribute in REMOVE_ATTRIBUTES:
+                    del child[attribute]
+                instruction_content.append(child)
+
+    instructions = instruction_content.select('div, p')
+
+    instructions = [instruction.text for instruction in instructions]
+
+    for instruction in instructions:
+        edited_instructions.append(
+            ''.join([i if ord(i) < 128 else ' ' for i in instruction]).strip())
+
+    # convert all instruction content to a string, and strip of unicode characters, add to our assignment object
+    return edited_instructions
+
+
+def getAssignmentFiles(url, browser):
+    browser.open(url)
+    assignment_page = browser.get_current_page()
+
+    files_sect = assignment_page.find('div', class_='fileDisplay')
+    files = []
+    if files_sect:
+        # Look in all of the a tags for the file information
+        for file in files_sect.find_all('a'):
+            # Grab the file information and add it to our files array
+            files.append({
+                'name': file.text,
+                'url': f"{BASE_URL}{file['href']}"
+            })
+    return files
